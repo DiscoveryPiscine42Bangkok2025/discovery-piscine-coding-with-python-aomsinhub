447d1bde83130818be26dd19fae833ccf21fc83f
aomsinhub <107758573+aomsinhub@users.noreply.github.com>
2025-09-16 20:13:10 +0700

    update 4.5

--- cell04/ex05/up_low.py
+++ cell04/ex05/up_low.py
@@ -1,10 +1,2 @@
 str = input()
-newStr = ""
-for c in str:
-    if c.isupper():
-        newStr += c.lower()
-    elif c.islower():
-        newStr += c.upper()
-    else:
-        newStr += c
-print(newStr)
+print(str.swapcase())
